5e27464caf3a8c523c56deb1a95772caad0ae6aa
maycondbraga <maycondbraga.s@gmail.com>
2024-12-15 14:42:03 -0300

    feat: initial commit

---
+++ src/test/java/com/md/oteldemo/OtelDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.md.oteldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
